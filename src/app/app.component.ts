e13bcc8f0ee9e092666083e2cc088af83bc1bd56
u8zfvf <chintan.pandya@siemens.com>
2019-12-10 17:45:17 +0530

    Updated CSS layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  closeMenu:boolean=true;
+  toggle(){
+    this.closeMenu=!this.closeMenu;
+  }
 }
